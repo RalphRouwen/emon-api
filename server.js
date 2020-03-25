0c838828d2630476ff71e4253feb28229faca273
Ralph Rouwen <ralph@rouwen.com>
2020-03-25 23:39:47 +0100

    fixed cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({credentials: true, origin: true}));
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
